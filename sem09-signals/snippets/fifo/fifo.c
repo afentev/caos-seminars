a8816f7c66b262965f863676dbc017ad94075596
k.afentev <k.afentev@tbank.ru>
2025-11-08 13:03:27 +0300

    sem09 signals added

---
+++ sem09-signals/snippets/fifo/fifo.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    mkfifo("./fifo_demo", 0644);
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        int fd = open("./fifo_demo", O_WRONLY);
+        dup2(fd, 1);
+        close(fd);
+        execlp(argv[1], argv[1], NULL);
+    } else {
+        // parent will read
+        int fd = open("./fifo_demo", O_RDONLY);
+        dup2(fd, 0);
+        close(fd);
+
+        int total_read = 0;
+        char buffer[4096];
+        int current_read;
+        while ((current_read = read(0, buffer, sizeof(buffer))) > 0) {
+            total_read += current_read;
+        }
+        printf("%d\n", total_read);
+        waitpid(pid, NULL, 0);
+    }
+}
